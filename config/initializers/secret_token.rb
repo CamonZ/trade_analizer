ff4221183bd6d80f7a3f6b96f548d26d661d281a
CamonZ <camonz@camonz.com>
2012-07-07 15:11:28 -0430

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TradeAnalizer::Application.config.secret_token = '9fe5047976e8c16c2b7302c52ffac2ed47f390238003d14bfeba5bf0bae86602bb064226df1c697befa5b822267568849a3d8112e2d9b25d45ffc6983a2ba563'
