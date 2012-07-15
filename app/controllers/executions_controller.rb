5701831f399a1e821cce353a1fbb86a2bd8d6c44
CamonZ <camonz@camonz.com>
2012-07-14 21:00:53 -0430

    wip

--- app/controllers/executions_controller.rb
+++ app/controllers/executions_controller.rb
@@ -1,10 +1,12 @@
 class ExecutionsController < ApplicationController
   def index
-    @executions_file = ExecutionsFile.new
+    @ef = ExecutionsFile.new
+    @executions = ExecutionsFile.all
   end
   
   def upload
-    debugger
-    logger.debug params
+    @executions_file = ExecutionsFile.new
+    
+    result = @executions_file.parse()
   end
 end
