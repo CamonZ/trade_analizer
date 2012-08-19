e172c272047d99bdac19a5458c0fed4a5eb25058
CamonZ <camonz@camonz.com>
2012-08-18 20:20:23 -0430

    added missing required field on the executions factory

--- spec/factories/executions.rb
+++ spec/factories/executions.rb
@@ -4,6 +4,7 @@ FactoryGirl.define do
   factory :execution do
     date "2012-07-07"
     execution_time "2012-07-07 16:47:24"
+    time_of_day "aftermarket"
     symbol "SINA"
     shares 100
     price 1.5
